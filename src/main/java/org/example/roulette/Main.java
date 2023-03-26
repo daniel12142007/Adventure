c6bca1fb0312b2a1efbb14214c1396f11490a21a
daniel12142007 <danijelahatzanov@gmail.com>
2023-03-26 22:45:15 +0600

    Initial commit

---
+++ src/main/java/org/example/roulette/Main.java
@@ -0,0 +1,19 @@
+package org.example.roulette;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        List<Users> users = new ArrayList<Users>();
+        System.out.println("Введите количество людей");
+        int count = scanner.nextInt();
+        for (int i = 0; i < count; i++) {
+
+        }
+    }
+}
---
+++ src/main/java/org/example/roulette/Users.java
@@ -0,0 +1,15 @@
+package org.example.roulette;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Users {
+    private String name;
+    private String group;
+}
